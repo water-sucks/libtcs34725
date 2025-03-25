288e77ed76d191c218a3883edb514acbf86918de
Varun Narravula <varun@snare.dev>
2025-03-25 02:35:16 -0700

    fix: fix up example flake to use subflake strategy

--- example/flake.nix
+++ example/flake.nix
@@ -1,34 +1,47 @@
 {
-  description = "CSC-615 Assignment 5 - TCS34725 color sensor C library";
+  description = "bruh";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    tcs34725.url = ../.;
   };
 
-  outputs = inputs @ {flake-parts, ...}:
+  outputs = inputs @ {
+    flake-parts,
+    tcs34725,
+    ...
+  }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux"];
 
-      perSystem = {pkgs, ...}: {
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        libtcs34725 = tcs34725.packages.${system}.default;
+      in {
         packages = let
-          tcs34725 = pkgs.callPackage (import ./package.nix) {};
-
-          example = pkgs.callPackage (import ./examples) {libtcs34725 = tcs34725;};
+          example = pkgs.callPackage ./package.nix {inherit libtcs34725;};
         in {
-          inherit tcs34725 example;
-          default = tcs34725;
+          inherit example;
+          default = example;
         };
 
         devShells.default = pkgs.mkShell {
           name = "tcs34725-shell";
+
           buildInputs = with pkgs; [
             gnumake
             gcc
 
             bear
             pkg-config
+
+            libtcs34725
           ];
         };
       };
